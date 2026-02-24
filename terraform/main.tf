32bda44de0178f8adfa33707ab2ccf9e0b50347c
Ayush soni <ayushsoni.6997@gmail.com>
2026-02-24 20:16:46 +0530

    feat: add ingress rule to allow traffic on port 3000 for easyshop security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,13 @@ resource "aws_security_group" "easyshop-sg" {
           cidr_blocks = ["0.0.0.0/0"]
           description = "Allow HTTPS traffic"
         }
+        ingress{
+          from_port   = 3000
+          to_port     = 3000
+          protocol  = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+          description = "Allow HTTPS traffic"
+        }
       egress{
           from_port   = 0
           to_port     = 0
